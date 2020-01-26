f13667a33743b1dbe0774d627ebb35c4cba8be1e
Mateusz Gdula <gdula.mat@gmail.com>
2020-01-26 23:56:51 +0100

    Refactoring of the main game class

--- src/js/SpaceInvader.js
+++ src/js/SpaceInvader.js
@@ -1,15 +1,5 @@
 /*
 To do:
-1. 
-2. 
-3. 
-4. 
-6. 
-9 
-11.
-12.
-10.
-5.
 13. Block show menu on back btn (mobiles)
 14. Refactor the menu
 8. New Ships, missles, more levels
@@ -32,8 +22,8 @@ class SpaceInvader {
         this.menu = document.querySelector('.menu');
         this.newGameBtn = document.querySelector('.menu__new-game');
         this.resumeBtn = document.querySelector('.menu__resume');
-        this.optionsBtn = document.querySelector('.menu__options');
-        this.exitBtn = document.querySelector('.menu__exit');
+        //this.optionsBtn = document.querySelector('.menu__options');
+        //this.exitBtn = document.querySelector('.menu__exit');
         
         this.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
         this.assets = assets;
@@ -45,33 +35,11 @@ class SpaceInvader {
         this.resumeBtn.addEventListener('click', this.resumeHandler.bind(this));
         document.addEventListener('keydown', this.pauseHandler.bind(this));
         
-        const eventName = this.isMobile ? 'click' : 'keydown';
+        const removeOverlayEvent = this.isMobile ? 'click' : 'keydown';
         this.removeOverlay = this.removeOverlay.bind(this);
-        const romoveOverlay = e => {
-            this.isMobile && this.gameContainer.requestFullscreen();
-            this.overlay.style.display = 'none';
-            document.removeEventListener(eventName, romoveOverlay);
-        }
-        document.addEventListener(eventName, romoveOverlay);
-        
-        this.gameContainer.addEventListener('fullscreenchange', async e => {
-            if (document.fullscreenElement !== null) {
-                try {
-                    await screen.orientation.lock('landscape');
-                } catch(e) {
-                    console.log(e);
-                }
-            } else {
-                this.game.pause();
-                this.menu.classList.add('open');
-            }
-        });
-    }
-    
-    removeOverlay(e) {
-        this.gameContainer.requestFullscreen();
-        this.overlay.style.display = 'none';
-        document.removeEventListener(eventName, romoveOverlay);
+        document.addEventListener(removeOverlayEvent, this.removeOverlay);
+
+        this.gameContainer.addEventListener('fullscreenchange', this.fullScreenHandler.bind(this));
     }
     
     runGame() {
@@ -87,12 +55,20 @@ class SpaceInvader {
         this.overlayText.textContent = this.isMobile ? 'Touch the screen to continue' : 'Press any key to continue';
     }
 
+    removeOverlay(e) {
+        const removeOverlayEvent = this.isMobile ? 'click' : 'keydown';
+        this.isMobile && this.gameContainer.requestFullscreen();
+        this.overlay.style.display = 'none';
+        document.removeEventListener(removeOverlayEvent, this.rmoveOverlay);
+    }
+
     newGameHandler() {
         this.game.newGame();
         this.menu.classList.toggle('open');
     }
 
     resumeHandler() {
+        this.isMobile && this.gameContainer.requestFullscreen();
         this.game.resume();
         this.menu.classList.toggle('open');
     }
@@ -101,6 +77,19 @@ class SpaceInvader {
         if(e.keyCode !== 27) return;
         this.game.playState ? this.game.pause() : this.game.resume();
         this.menu.classList.toggle('open');
+    }
+
+    async fullScreenHandler(e) {
+        if (document.fullscreenElement !== null) {
+            try {
+                await screen.orientation.lock('landscape');
+            } catch(e) {
+                console.log(e);
+            }
+        } else {
+            this.game.pause();
+            this.menu.classList.add('open');
+        }
     }
 }
 
